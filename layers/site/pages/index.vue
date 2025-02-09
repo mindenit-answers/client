06e116ae7022e84b6cf7ccd47603db89b0fc0c7d
perkinson1251 <tttrashutin@gmail.com>
2025-02-09 16:28:04 +0200

    chore: add basic modules and ui library

--- layers/site/pages/index.vue
+++ layers/site/pages/index.vue
@@ -1,7 +1,5 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Hello world!    
-  </div>
+  <Heading size="small">Hello world!</Heading>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,41 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  extends: [
-    './core',
-    './layers/site'
+  extends: ['./core', './layers/site'],
+  modules: [
+    '@mindenit/ui/nuxt',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+    '@vee-validate/nuxt',
+    '@nuxtjs/seo',
+    'nuxt-gtag',
+    '@nuxt/eslint',
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  imports: {
+    dirs: ['./core/types', './layers/**/types'],
+  },
+  css: ['~/core/assets/css/main.css'],
+  icon: {
+    provider: 'iconify',
+    serverBundle: {
+      collections: ['lucide'],
+    },
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode',
+  },
+  gtag: {
+    id: 'G-3FX2TXSLDS',
+  },
 })
