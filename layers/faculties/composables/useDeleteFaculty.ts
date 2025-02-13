804718f0895b73529e0176fb195571e873297dc6
perkinson1251 <tttrashutin@gmail.com>
2025-02-13 17:02:13 +0200

    feat: implement create, update, and delete faculty mutations

---
+++ layers/faculties/composables/useDeleteFaculty.ts
@@ -0,0 +1,18 @@
+import type { RequestHeaders } from '@mindenit/answers-kit'
+import { useMutation, useQueryClient } from '@tanstack/vue-query'
+
+interface Args {
+  id: number
+  headers: RequestHeaders
+}
+
+export const useDeleteFaculty = () => {
+  const { $answersKit } = useNuxtApp()
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (data: Args) => $answersKit.faculties.deleteOne(data),
+    onSettled: () => queryClient.invalidateQueries({ queryKey: ['faculties'] }),
+    mutationKey: ['deleteFaculty'],
+  })
+}
