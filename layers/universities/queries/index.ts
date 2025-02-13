86b53d5677ed8a6b0ce7ba0659ed4a62001f1ea2
perkinson1251 <tttrashutin@gmail.com>
2025-02-13 12:11:20 +0200

    feat: add queries to universities layer

---
+++ layers/universities/queries/index.ts
@@ -0,0 +1,42 @@
+import type { SortingOptions } from '@mindenit/answers-kit'
+import { queryOptions } from '@tanstack/vue-query'
+
+interface UniversitiesOptionsArgs {
+  order: Ref<SortingOptions['order']>
+  sortBy: Ref<SortingOptions['sortBy']>
+}
+
+const universityOptions = (id: number) => {
+  const { $answersKit } = useNuxtApp()
+
+  return queryOptions({
+    queryKey: ['university', id],
+    queryFn: () => $answersKit.universities.findOne(id),
+  })
+}
+
+const universitiesOptions = (args: UniversitiesOptionsArgs) => {
+  const { $answersKit } = useNuxtApp()
+
+  return queryOptions({
+    queryKey: ['universities', args.sortBy.value, args.order.value],
+    queryFn: () =>
+      $answersKit.universities.findMany({
+        sorting: {
+          order: args.order.value,
+          sortBy: args.sortBy.value,
+        },
+      }),
+  })
+}
+
+const universityFacultiesOptions = (id: number) => {
+  const { $answersKit } = useNuxtApp()
+
+  return queryOptions({
+    queryKey: ['universityFaculties', id],
+    queryFn: () => $answersKit.universities.findOneFaculties({ id }),
+  })
+}
+
+export { universitiesOptions, universityFacultiesOptions, universityOptions }
