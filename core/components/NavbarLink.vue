81d05d948d3f556434d37d00c13587cf1e9f79c1
perkinson1251 <tttrashutin@gmail.com>
2025-03-20 11:31:23 +0200

    feat: enhance navbar component with improved styling

--- core/components/NavbarLink.vue
+++ core/components/NavbarLink.vue
@@ -4,9 +4,7 @@ interface Props {
   icon: string
   title: string
 }
-
 const props = defineProps<Props>()
-
 const route = useRoute()
 const isActive = computed(() => route.path === props.to)
 </script>
@@ -15,11 +13,31 @@ const isActive = computed(() => route.path === props.to)
   <TooltipProvider>
     <TooltipRoot>
       <TooltipTrigger as-child>
-        <NuxtLink :to="to" :title="title" :aria-label="title">
+        <NuxtLink
+          :to="to"
+          :title="title"
+          :aria-label="title"
+          class="sm:contents max-sm:flex-1"
+        >
           <div
+            class="max-sm:hidden"
             :class="{ 'bg-fiord-200 dark:bg-fiord-800 rounded-lg': isActive }"
           >
-            <IconButton variant="ghost" :icon="icon" />
+            <Button variant="ghost" class="hover:cursor-pointer">
+              <Icon :name="icon" size="18px" /> {{ title }}
+            </Button>
+          </div>
+
+          <div
+            class="sm:hidden flex flex-col items-center justify-center p-2 dark:text-white text-black text-sm"
+            :class="{ 'bg-fiord-200 dark:bg-fiord-800 rounded-lg': isActive }"
+          >
+            <div
+              class="w-full mb-1 inline-flex items-center justify-center px-2"
+            >
+              <Icon :name="icon" size="20px" />
+            </div>
+            <span>{{ title }}</span>
           </div>
         </NuxtLink>
       </TooltipTrigger>
