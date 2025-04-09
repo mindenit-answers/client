62a71afa4dd411fd6d01b24dd23f238e9f7ed832
perkinson1251 <tttrashutin@gmail.com>
2025-04-09 12:17:01 +0300

    fix: adjust padding in default layout

--- layers/site/layouts/default.vue
+++ layers/site/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col w-full relative">
     <div class="flex flex-col pt-5 container mx-auto px-4 h-dvh gap-5">
       <TheNavbar />
-      <div class="flex flex-col md:pb-5 pb-20 gap-5">
+      <div class="flex flex-col md:pb-5 pb-24 gap-5">
         <slot />
       </div>
     </div>
