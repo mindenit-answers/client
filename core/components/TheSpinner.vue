6c79656e450bcec51dbeaefc7db75257b20396b0
perkinson1251 <tttrashutin@gmail.com>
2025-04-10 12:16:10 +0300

    fix: adjust spinner height for better responsiveness on smaller screens

--- core/components/TheSpinner.vue
+++ core/components/TheSpinner.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="h-[80vh] w-full flex items-center justify-center" role="status">
+  <div
+    class="h-[80vh] max-md:h-[65vh] w-full flex items-center justify-center"
+    role="status"
+  >
     <div
       class="animate-spin rounded-full h-12 w-12 border-4 border-royal-blue-500 border-t-transparent"
     ></div>
