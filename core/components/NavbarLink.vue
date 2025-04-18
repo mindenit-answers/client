9aad1d315322766e8dabc52e267d9b73f9342c0a
perkinson1251 <tttrashutin@gmail.com>
2025-04-18 15:31:59 +0300

    refactor: simplify navbar link component

--- core/components/NavbarLink.vue
+++ core/components/NavbarLink.vue
@@ -4,25 +4,26 @@ interface Props {
   icon: string
   title: string
 }
-const props = defineProps<Props>()
-const route = useRoute()
-const isActive = computed(() => route.path === props.to)
+
+defineProps<Props>()
 </script>
 
 <template>
-  <NuxtLink :to="to" :aria-label="title" class="sm:contents max-sm:flex-1">
+  <NuxtLink
+    :to="to"
+    :aria-label="title"
+    class="sm:contents max-sm:flex-1 group"
+    active-class="active-link"
+  >
     <div
-      class="max-sm:hidden"
-      :class="{ 'bg-fiord-200 dark:bg-fiord-800 rounded-lg': isActive }"
+      class="max-sm:hidden group-[.active-link]:bg-fiord-200 group-[.active-link]:dark:bg-fiord-800 group-[.active-link]:rounded-lg"
     >
       <Button variant="ghost" class="hover:cursor-pointer">
         <Icon :name="icon" size="18px" /> {{ title }}
       </Button>
     </div>
-
     <div
-      class="sm:hidden flex flex-col items-center justify-center p-2 dark:text-white text-black text-sm"
-      :class="{ 'bg-fiord-200 dark:bg-fiord-800 rounded-lg': isActive }"
+      class="sm:hidden flex flex-col items-center justify-center p-2 dark:text-white text-black text-sm group-[.active-link]:bg-fiord-200 group-[.active-link]:dark:bg-fiord-800 group-[.active-link]:rounded-lg"
     >
       <div class="w-full mb-1 inline-flex items-center justify-center px-2">
         <Icon :name="icon" size="20px" />
