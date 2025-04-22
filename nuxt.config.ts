ab25c8f807854ae5989f0bb1ded67a0d85fbcd73
perkinson1251 <tttrashutin@gmail.com>
2025-04-22 15:07:25 +0300

    refactor: remove @mindenit/ui dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['./core', './layers/site'],
   modules: [
-    '@mindenit/ui/nuxt',
     '@nuxt/icon',
     '@nuxt/image',
     '@vueuse/nuxt',
