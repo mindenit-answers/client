82a706b25b8ebc44fa00d49c9000b56b4cfe7403
perkinson1251 <tttrashutin@gmail.com>
2025-04-27 11:02:14 +0300

    fix: reduce transition duration for scrollbar progress

--- layers/tests/components/Scrollbar.vue
+++ layers/tests/components/Scrollbar.vue
@@ -5,7 +5,7 @@ const { progress } = useScrollProgress()
 <template>
   <div class="fixed top-0 left-0 w-full h-1 z-50">
     <div
-      class="h-full bg-primary transition-all duration-300"
+      class="h-full bg-primary transition-all duration-100"
       :style="{ width: `${progress}%` }"
     ></div>
   </div>
