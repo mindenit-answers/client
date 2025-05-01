3c0555a94580b7b89025a69adc44e69e82d04c3e
Roman <47219330+perkinson1251@users.noreply.github.com>
2025-05-01 13:40:48 +0300

    chore: repo configuration (#19)
    * chore: update readme
    
    * chore: add pre-commit hooks
    
    * chore: update eslint config
    
    * chore: enchance package.json
    
    * chore: add guidelines and code of conduct
    
    * chore: add templates
    
    * chore: add feature change pull request template

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,32 @@
 import prettier from 'eslint-config-prettier'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
-  files: ['**/**/*.{js,ts,vue,mjs,mts}'],
-  rules: {
-    'vue/multi-word-component-names': ['off', 'always'],
-    'vue/no-v-html': ['off', 'always'],
+export default withNuxt(
+  {
+    files: ['**/**/*.{js,ts,vue,mjs,mts}'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/require-default-prop': 'off',
+    },
+    ignores: [
+      'dist',
+      '.nuxt',
+      'node_modules',
+      '.output',
+      '.git',
+      'build',
+      'LICENSE',
+      '*.md',
+      'public',
+      'package-lock.json',
+    ],
+    ...prettier,
   },
-  ignores: [
-    'dist',
-    '.nuxt',
-    'node_modules',
-    '.output',
-    '.git',
-    'build',
-    'LICENSE',
-    '*.md',
-    'public',
-    'package-lock.json',
-  ],
-  ...prettier,
-})
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/require-default-prop': 'off',
+      'vue/no-v-html': 'off',
+    },
+  },
+)
