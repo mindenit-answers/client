bbf9844be9b27bfe54f5d77f65c8eaafb8ea0373
perkinson1251 <tttrashutin@gmail.com>
2025-05-19 16:10:59 +0300

    refactor: streamline markdown rendering in question card component

--- core/composables/useMarkdown.ts
+++ core/composables/useMarkdown.ts
@@ -4,12 +4,21 @@ import MarkdownIt from 'markdown-it'
 import tm from 'markdown-it-texmath'
 
 export const useMarkdown = () => {
-  const markdown = new MarkdownIt({ html: true })
+  const markdown = new MarkdownIt({
+    html: true,
+    breaks: true,
+  })
     .disable(['heading', 'link', 'image', 'list', 'blockquote'])
     .use(tm, {
       delimiters: 'dollars',
       engine: katex,
     })
+
+  const originalRender = markdown.render.bind(markdown)
+  markdown.render = (src: string) => {
+    const processedSrc = src.replace(/\n/g, '<br>\n')
+    return originalRender(processedSrc)
+  }
 
   return { markdown }
 }
