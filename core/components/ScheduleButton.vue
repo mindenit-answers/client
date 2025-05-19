ba2836a75a8491942ef791418694bf5979d9a59f
perkinson1251 <tttrashutin@gmail.com>
2025-05-19 17:12:38 +0300

    feat: add link to schedule in navbar

---
+++ core/components/ScheduleButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child>
+        <NuxtLink
+          to="https://sh.mindenit.org"
+          target="_blank"
+          class="flex items-center justify-center"
+        >
+          <Icon name="mindenit:schedule" class="!size-5" />
+        </NuxtLink>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Перейти на сайт з розкладом</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
