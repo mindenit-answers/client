061889bd6c6f7382f24233d43a3a90672a68c8ed
perkinson1251 <tttrashutin@gmail.com>
2025-07-23 11:41:46 +0300

    feat: change backend url

--- core/plugins/answers-kit.ts
+++ core/plugins/answers-kit.ts
@@ -1,6 +1,8 @@
 import { AnswersKit } from '@mindenit/answers-kit'
 
-const answersKit = new AnswersKit()
+const answersKit = new AnswersKit({
+  apiUrl: 'https://answers.pp.ua/api',
+})
 
 export default defineNuxtPlugin(() => {
   return {
